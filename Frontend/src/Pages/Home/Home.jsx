5f125e1cdb146db1109bb0bc503de955110b28d5
instadotanalytics <instadotanalytics@gmail.com>
2026-07-13 12:44:41 +0530

    Header Nad Bnannenr

---
+++ Frontend/src/Pages/Home/Home.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Header from '../Layout/Header/Header'
+import HomeBanner from '../../Components/Banner/HomeBanner'
+
+const Home = () => {
+  return (
+    <div>
+        <Header/>
+        <HomeBanner/>
+    </div>
+  )
+}
+
+export default Home
