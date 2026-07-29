65ad57d4de937963919b8d3b15d04bdb6983e7b6
instadotanalytics <instadotanalytics@gmail.com>
2026-07-29 12:09:41 +0530

    sdfvbg

--- Frontend/src/Pages/Home/Home.jsx
+++ Frontend/src/Pages/Home/Home.jsx
@@ -1,32 +1,15 @@
-
-import React from 'react'
-import HomeBanner from '../../Components/Banner/HomeBanner'
-import Footer from '../Layout/Footer/Footer'
-import ShopByCategory from '../ShopByCategory/Shopbycategory'
-import ShopCardCategory from '../../Components/ShopCard/ShopCardCategory'
-import NewCollections from '../../Components/ShopCard/NewCollections'
-import GiftGuide from '../../Components/ShopCard/GiftGuide'
-import Offers from '../../Components/ShopCard/Offers'
-// src/Pages/Home/Home.jsx
-
 import React from "react";
+
 import HomeBanner from "../../Components/Banner/HomeBanner";
-import Footer from "../Layout/Footer/Footer";
 import ShopByCategory from "../ShopByCategory/Shopbycategory";
 import ShopCardCategory from "../../Components/ShopCard/ShopCardCategory";
 import NewArrivalsBanner from "../../Components/Banner/NewArrivalsBanner";
 import NewCollections from "../../Components/ShopCard/NewCollections";
+import Footer from "../Layout/Footer/Footer";
 
 const Home = () => {
   return (
     <div>
-       <HomeBanner/>
-        <ShopByCategory/>
-        <ShopCardCategory/>
-        <NewCollections/>
-        <GiftGuide/>
-        <Offers/>
-        <Footer/>
       <HomeBanner />
       <ShopByCategory />
       <ShopCardCategory />
