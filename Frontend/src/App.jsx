ea1ec562ebca130158d47e063858e488ec05900c
instadotanalytics <instadotanalytics@gmail.com>
2026-08-04 12:41:37 +0530

    sdv

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -309,9 +309,9 @@ const App = () => {
           <Route
             path={ROUTES.SHOP}
             element={
-              <LayoutWithHeader>
+             
                 <Shop />
-              </LayoutWithHeader>
+              
             }
           />
 
